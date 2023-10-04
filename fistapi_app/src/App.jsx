c29870d81a58ec0748aaa01ba405aa1a9560398e
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-04 17:44:44 +0900

    [add] front files

---
+++ fistapi_app/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import axios from "axios";
+
+function App() {
+	const [data, setData] = React.useState();
+	const url = "http://127.0.0.1:8000";
+
+	const GetData = () => {
+		axios.get(url).then((res) => {
+			setData(res.data);
+		});
+	};
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+		</div>
+	);
+}
+
+export default App;
