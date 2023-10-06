3d97bc28fbb743c6a5ffee66589ec9504ac0b884
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-07 08:12:30 +0900

    [add] props in App about tiyle

--- fistapi_app/src/App.jsx
+++ fistapi_app/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import Footer from "./compornents/Footer/footter";
 import Navbar from "./compornents/Navbar/nav";
 
-function App() {
+function App(props) {
 	const [data, setData] = React.useState();
 	const url = "http://127.0.0.1:8000";
 
@@ -12,9 +12,15 @@ function App() {
 			setData(res.data);
 		});
 	};
+
+  // title
+  const subject = props.subject;
 	return (
-		<div className="lessonApp stack-large">
+		<div className="App stack-large">
       <Navbar />
+
+        <h1>{ subject } Lesson!</h1>
+
         <div>ここに処理を書いていきます</div>
         {data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
       <Footer />
