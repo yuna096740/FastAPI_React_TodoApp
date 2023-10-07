4fa6666110a3ce2f1049e47401bfaecbb97ba915
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-07 09:05:37 +0900

    [add] AddTaskFrom

--- fistapi_app/src/App.jsx
+++ fistapi_app/src/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { nanoid } from "nanoid";
 import Footer from "./compornents/Footer/footter";
 import Navbar from "./compornents/Navbar/nav";
+import AddTaskForm from "./compornents/From/AddTaskFrom";
 
 function App(props) {
 	const [data, setData] = React.useState();
@@ -12,14 +14,24 @@ function App(props) {
 			setData(res.data);
 		});
 	};
-
   // title
   const subject = props.subject;
+  const [tasks, setTasks] = useState(props.tasks)
+
+  // Taskの追加処理
+  function addTask(name) {
+    const newTask = { id: `todo-${ nanoid() }`, name, completed: false};
+    // スプレッド演算子(...)
+    setTasks([...tasks, newTask]);
+  }
+
 	return (
 		<div className="App stack-large">
       <Navbar />
 
         <h1>{ subject } Lesson!</h1>
+
+        <AddTaskForm />
 
         <div>ここに処理を書いていきます</div>
         {data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
---
+++ fistapi_app/src/compornents/From/AddTaskFrom.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function AddTaskForm(props) {
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault(); // デフォルトの送信動作を防ぐ
+    props.addTask(name);
+    setName("");
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What do you want to do?
+        </label>
+      </h2>
+
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={ name }
+        onChange={ handleChange }
+      />
+
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add Task
+      </button>
+
+    </form>
+  )
+}
+
+export default AddTaskForm;
