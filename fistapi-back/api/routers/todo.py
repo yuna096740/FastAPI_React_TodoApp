a5f0a42e69736cc4373cbb620df13683ef73bddb
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-09 16:03:59 +0900

    [add] router about create_todo

--- fistapi-back/api/routers/todo.py
+++ fistapi-back/api/routers/todo.py
@@ -6,15 +6,18 @@ router = APIRouter()
 
 @router.get("/todos", response_model=List[todo_schema.Todo])
 async def list_todos():
-    return [todo_schema.Todo(id=1, title="todo test1", done=False)]
+    return [todo_schema.Todo(id=1, title="todo test1", done=True)]
 
-@router.post("/todos")
-async def create_todo():
-    pass
+
+@router.post("/todos", response_model=todo_schema.TodoCreateResponse)
+async def create_todo(todo_body: todo_schema.TodoCreate):
+    return todo_schema.TodoCreateResponse(id=1, **todo_body.dict())
+
 
 @router.put("/todos/{todo_id}")
 async def update_todo():
     pass
+
 
 @router.delete("/todos/{todo_id}")
 async def delete_todo():
--- fistapi-back/api/schemas/todo.py
+++ fistapi-back/api/schemas/todo.py
@@ -1,8 +1,24 @@
 from typing import Optional
-
 from pydantic import BaseModel, Field
 
-class Todo(BaseModel):
+class TodoBase(BaseModel):
+    title: Optional[str] = Field(None, example="FastAPI Lesson")
+
+
+class TodoCreate(TodoBase):
+    pass
+
+
+class TodoCreateResponse(TodoCreate):
     id: int
-    title: Optional[str] = Field(None, example="React Lesson")
+
+    class Config:
+        orm_mode = True # DBと接続する際に使用
+
+
+class Todo(TodoBase):
+    id: int
     done: bool = Field(False, description="done")
+
+    class Config:
+        orm_mode = True # DBと接続する際に使用
