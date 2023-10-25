182f162c8fbfb04ed49540fa041890979c327283
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-25 09:51:41 +0900

    [add] Auth faile and directory

---
+++ fastapi-front/src/compornents/Auth/AuthForm.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function AuthForm({ onLogin }) {
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+}
