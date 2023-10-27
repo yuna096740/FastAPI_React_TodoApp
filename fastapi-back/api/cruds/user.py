f3a83de2854f36635e1d3a7906b148acb25de624
yuna096740 <yyuusseeii1111@gmail.com>
2023-10-27 09:01:01 +0900

    [add] user in cruds

---
+++ fastapi-back/api/cruds/user.py
@@ -0,0 +1,31 @@
+from paalib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from api.models.user import user_models
+
+pwd_context = CryptContext(schemas=["bcrypt"], deprecated="auto")
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(user_models).filter(user_models.User.email == email).first()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(
+        db: Session,
+        email: str,
+        password: str,
+        expire: int,
+        reuse: bool):
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return True
+
+
+def get_password_hash(password):
+    return pwd_context.pwd_context.hash(password)
